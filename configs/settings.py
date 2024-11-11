2992390462fd8e5cfa5b0d9482bc0106557af638
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-11-11 21:56:59 +0300

    Update settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -1,5 +1,5 @@
 token = '6731828059:AAF_ArYcnkrHhTo89kxe99zERmG0-H2BSNM'
-dir = r'C:\\WonderfulBot-main'
+dir = r'C:\Users\user\Desktop\WonderfulBot-main>'
 version = '3.3'
 admins_id = [@neki4crystal]
 
